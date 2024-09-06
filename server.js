69fa889c8228521f714a50863ba050ef3c117e5f
Godspower <chimabest70@gmail.com>
2024-09-06 12:02:26 +0100

    adding cors

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ mongoose
 // middlewares
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 app.use("/api", authRoute);
 app.use("/api", postRoute);
 
